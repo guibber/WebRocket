09add8cf6dafb1b3aac7fc705094fc4ca57090cb
Geoffrey <geoffrey@guibber.com>
2017-08-30 18:09:24 -0700

    added IntegrationTests

--- WebRocket/IRocket.cs
+++ WebRocket/IRocket.cs
@@ -1,3 +1,10 @@
-﻿namespace WebRocket {
-  public interface IRocket { }
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebRocket {
+  public interface IRocket {
+    Task<RocketResult> ReceiveStreamAsync(MemoryStream stream, CancellationToken token);
+    Task<RocketResult> SendStreamAsync(MemoryStream stream, CancellationToken token);
+  }
 }
---
+++ WebRocketTests/IntegrationTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebRocket;
+using WebRocket.Wrappers;
+
+namespace WebRocketTests {
+  [TestFixture]
+  public class IntegrationTests {
+    [Test]
+    public async Task TestOneNewConnection() {
+      await ExecuteConnectTransferAndClose();
+    }
+
+    [Test]
+    public void TestMulitpleNewConnections() {
+      Task.WaitAll(ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose());
+    }
+
+    [Test]
+    public void TestWithLotsOfNewConnections() {
+      Task.WaitAll(ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(),
+                   ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(),
+                   ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(),
+                   ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose(), ExecuteConnectTransferAndClose());
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mSource = new CancellationTokenSource();
+      mListener = new RocketListener(new HttpListener(), new RocketAcceptor());
+      mAcceptingAsync = mListener.StartAcceptingAsync("http://localhost:9090/Test/", async (rocket, token) => {
+                                                                                       using (var stream = new MemoryStream()) {
+                                                                                         await rocket.ReceiveStreamAsync(stream, token);
+                                                                                         await rocket.SendStreamAsync(stream, token);
+                                                                                         await rocket.ReceiveStreamAsync(stream, token);
+                                                                                       }
+                                                                                     }, mSource.Token).GetAwaiter();
+    }
+
+    [TearDown]
+    public void DoTeardown() {
+      mSource.Cancel();
+      mListener.Stop();
+      mAcceptingAsync.GetResult();
+    }
+
+    private static async Task ExecuteConnectTransferAndClose() {
+      var source = new CancellationTokenSource();
+      var buffer = Encoding.UTF8.GetBytes("hello");
+      var rocket = new ClientRocket(new ClientWebSocketBuilder());
+      Assert.True(await rocket.ConnectAsync(new Uri("ws://localhost:9090/Test/"), source.Token));
+      Assert.That(await rocket.SendStreamAsync(new MemoryStream(buffer, 0, buffer.Length, false, true), source.Token), Is.EqualTo(new RocketResult()));
+      using (var stream = new MemoryStream()) {
+        await rocket.ReceiveStreamAsync(stream, source.Token);
+        Assert.That(stream.GetBuffer().Take(buffer.Length), Is.EqualTo(buffer));
+      }
+      await rocket.CloseAsync(source.Token);
+    }
+
+    private RocketListener mListener;
+    private CancellationTokenSource mSource;
+    private TaskAwaiter mAcceptingAsync;
+  }
+}
