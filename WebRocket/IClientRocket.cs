92ba3a6d7fa50c2c76e2c9fd84d6311b0123a2e1
Geoffrey <geoffrey@guibber.com>
2017-08-31 20:27:06 -0700

    filled out missing parts of IClientRocket and IRocket ifaces

--- WebRocket/IClientRocket.cs
+++ WebRocket/IClientRocket.cs
@@ -1,3 +1,14 @@
-﻿namespace WebRocket {
-  public interface IClientRocket { }
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebRocket {
+  public interface IClientRocket {
+    bool IsOpen { get; }
+    Task<bool> ConnectAsync(Uri uri, CancellationToken token);
+    Task<RocketResult> ReceiveStreamAsync(MemoryStream stream, CancellationToken token);
+    Task<RocketResult> SendStreamAsync(MemoryStream stream, CancellationToken token);
+    Task CloseAsync(CancellationToken token);
+  }
 }
--- WebRocket/IRocket.cs
+++ WebRocket/IRocket.cs
@@ -4,7 +4,9 @@ using System.Threading.Tasks;
 
 namespace WebRocket {
   public interface IRocket {
+    bool IsOpen { get; }
     Task<RocketResult> ReceiveStreamAsync(MemoryStream stream, CancellationToken token);
     Task<RocketResult> SendStreamAsync(MemoryStream stream, CancellationToken token);
+    Task CloseAsync(CancellationToken token);
   }
 }
