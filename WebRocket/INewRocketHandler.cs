5caa091d21aacdff4759ef298fbcbdaee3c1f62e
Geoffrey <geoffrey@guibber.com>
2017-09-05 11:02:18 -0700

    added INewRocketHandler iface

---
+++ WebRocket/INewRocketHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebRocket {
+  public interface INewRocketHandler {
+    Task HandleAsync(IRocket rocket, CancellationToken token);
+  }
+}
