152b124fbe6004350ee7d7cdde9a7d4d00a832e8
Geoffrey <geoffrey@guibber.com>
2018-08-11 18:37:26 -0700

    added WebRocket.Server.Autofac project

---
+++ WebRocket.Server.Autofac/Module.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using WebRocket.Server.Wrappers;
+
+namespace WebRocket.Server.Autofac {
+  public class Module {
+    public void Register(ContainerBuilder builder) {
+      builder
+        .RegisterType<RocketListener>()
+        .As<IRocketListener>()
+        .SingleInstance();
+
+      builder
+        .RegisterType<HttpListener>()
+        .As<IHttpListerner>()
+        .SingleInstance();
+
+      builder
+        .RegisterType<RocketAcceptor>()
+        .As<IRocketAcceptor>()
+        .SingleInstance();
+    }
+  }
+}
