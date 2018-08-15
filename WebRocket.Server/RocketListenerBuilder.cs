e2ac815a779a5910cc6ff5fedbe11307de39de76
Geoffrey <geoffrey@guibber.com>
2018-08-14 22:32:50 -0700

    upgraded RocketListenerBuilder to allow custom IObserver

--- WebRocket.Server/RocketListenerBuilder.cs
+++ WebRocket.Server/RocketListenerBuilder.cs
@@ -3,7 +3,11 @@
 namespace WebRocket.Server {
   public class RocketListenerBuilder {
     public static RocketListener Build() {
-      return new RocketListener(new HttpListener(), new RocketAcceptor(), new NoOpObserver());
+      return Build(new NoOpObserver());
+    }
+
+    public static RocketListener Build(IObserver observer) {
+      return new RocketListener(new HttpListener(), new RocketAcceptor(), observer);
     }
   }
 }
